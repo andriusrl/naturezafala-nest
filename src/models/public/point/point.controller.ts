54aed1fa033ea0345f474642659793517a21a92c
Andrius <andrius.rochalazarino@gmail.com>
2023-11-21 10:17:06 -0400

    fix: remove register access in get point

--- src/models/public/point/point.controller.ts
+++ src/models/public/point/point.controller.ts
@@ -1,15 +1,15 @@
 import {
-    Headers,
-    Body,
-    Controller,
-    Get,
-    Inject,
-    Param,
-    Post,
-    UseGuards,
-    Delete,
-    Patch,
-    Ip
+  Headers,
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  UseGuards,
+  Delete,
+  Patch,
+  Ip,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { PointService } from './point.service';
@@ -21,71 +21,89 @@ import { AccessHelper } from 'src/helpers/access.helper';
 
 @Controller('point')
 export class PointController {
-    constructor(
-        @Inject(PointService)
-        private readonly service: PointService,
-        private readonly accessService: AccessService,
-    ) { }
+  constructor(
+    @Inject(PointService)
+    private readonly service: PointService,
+    private readonly accessService: AccessService,
+  ) {}
 
-    @Get('/')
-    async findAll(): Promise<Point[]> {
-        return this.service.findAll();
-    }
+  @Get('/')
+  async findAll(): Promise<Point[]> {
+    return this.service.findAll();
+  }
 
-    @Get('/:id')
-    async findOne(
-        @Headers('authorization') authorization: string,
-        @Ip() ip,
-        @Param('id') id,
-    ) {
-        const response = await this.service.findOne(+id);
+  @Get('/:id')
+  async findOne(
+    // @Headers('authorization') authorization: string,
+    @Ip() ip,
+    @Param('id') id,
+  ) {
+    const response = await this.service.findOne(+id);
 
-        await this.accessService.create(AccessHelper.ACTION.VIEWED, 'point', authorization, ip);
+    // await this.accessService.create(
+    //   AccessHelper.ACTION.VIEWED,
+    //   'point',
+    //   authorization,
+    //   ip,
+    // );
 
-        return response
-    }
+    return response;
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Post('')
-    async createPoint(
-        @Headers('authorization') authorization: string,
-        @Ip() ip,
-        @Body() point: CreatePointDto,
-    ) {
+  @UseGuards(AuthGuard('jwt'))
+  @Post('')
+  async createPoint(
+    @Headers('authorization') authorization: string,
+    @Ip() ip,
+    @Body() point: CreatePointDto,
+  ) {
+    const response = this.service.createPoint(point, authorization);
 
-        const response = this.service.createPoint(point, authorization);
+    await this.accessService.create(
+      AccessHelper.ACTION.ADDED,
+      'point',
+      authorization,
+      ip,
+    );
 
-        await this.accessService.create(AccessHelper.ACTION.ADDED, 'point', authorization, ip);
+    return response;
+  }
 
-        return response
-    }
+  @UseGuards(AuthGuard('jwt'))
+  @Patch('')
+  async updatePoint(
+    @Headers('authorization') authorization: string,
+    @Ip() ip,
+    @Body() point: UpdatePointDto,
+  ) {
+    const response = await this.service.update(point, authorization);
 
-    @UseGuards(AuthGuard('jwt'))
-    @Patch('')
-    async updatePoint(
-        @Headers('authorization') authorization: string,
-        @Ip() ip,
-        @Body() point: UpdatePointDto,
-    ) {
-        const response = await this.service.update(point, authorization);
+    await this.accessService.create(
+      AccessHelper.ACTION.UPDATE,
+      'point',
+      authorization,
+      ip,
+    );
 
-        await this.accessService.create(AccessHelper.ACTION.UPDATE, 'point', authorization, ip);
+    return response;
+  }
 
-        return response
-    }
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/:id')
+  async deleteFile(
+    @Headers('authorization') authorization: string,
+    @Ip() ip,
+    @Param('id') id,
+  ) {
+    const response = await this.service.delete(+id, authorization);
 
-    @UseGuards(AuthGuard('jwt'))
-    @Delete('/:id')
-    async deleteFile(
-        @Headers('authorization') authorization: string,
-        @Ip() ip,
-        @Param('id') id,
-    ) {
-        const response = await this.service.delete(+id, authorization);
+    await this.accessService.create(
+      AccessHelper.ACTION.DELETE,
+      'point',
+      authorization,
+      ip,
+    );
 
-        await this.accessService.create(AccessHelper.ACTION.DELETE, 'point', authorization, ip);
-
-        return response
-    }
-
+    return response;
+  }
 }
