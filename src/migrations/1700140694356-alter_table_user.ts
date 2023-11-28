a43e66e35c9ef18bf0e569b99a825cdaa086775c
Andrius <andrius.rochalazarino@gmail.com>
2023-11-28 08:19:20 -0400

    fix: migration id user

--- src/migrations/1700140694356-alter_table_user.ts
+++ src/migrations/1700140694356-alter_table_user.ts
@@ -6,14 +6,14 @@ export class AlterTableUser1700140694356 implements MigrationInterface {
             ALTER TABLE public."user" ADD status boolean NOT NULL;          
             
             INSERT INTO public."user"
-                (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(1, 'Andrius Administrador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.administrador@email.com', '221133', 1, true);
+                ("name", birth_date, fone, cpf, email, "password", "type", "status")
+                VALUES('Andrius Administrador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.administrador@email.com', '221133', 1, true);
             INSERT INTO public."user"
-                (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(2, 'Andrius Moderador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.moderador@email.com', '221133', 2, true);
+                ("name", birth_date, fone, cpf, email, "password", "type", "status")
+                VALUES('Andrius Moderador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.moderador@email.com', '221133', 2, true);
             INSERT INTO public."user"
-                (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(3, 'Andrius Usuário', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius@email.com', '221133', 3, true);
+                ("name", birth_date, fone, cpf, email, "password", "type", "status")
+                VALUES('Andrius Usuário', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius@email.com', '221133', 3, true);
         `);
   }
 
