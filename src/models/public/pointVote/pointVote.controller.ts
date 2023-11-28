7e3ce88ef62c5792503831f84526ff92d79676a1
Andrius <andrius.rochalazarino@gmail.com>
2023-11-28 09:22:28 -0400

    fix: findAll vote by point

--- src/models/public/pointVote/pointVote.controller.ts
+++ src/models/public/pointVote/pointVote.controller.ts
@@ -1,17 +1,17 @@
 import {
-    Body,
-    Headers,
-    Controller,
-    Delete,
-    Get,
-    Inject,
-    Param,
-    Patch,
-    Post,
-    UploadedFile,
-    UseGuards,
-    UseInterceptors,
-    Ip,
+  Body,
+  Headers,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+  Ip,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { PointVote } from './entities/pointVote.entity';
@@ -22,49 +22,63 @@ import { AccessHelper } from 'src/helpers/access.helper';
 
 @Controller('pointvote')
 export class PointVoteController {
-    constructor(
-        @Inject(PointVoteService)
-        private readonly service: PointVoteService,
-        private readonly accessService: AccessService,
-    ) { }
+  constructor(
+    @Inject(PointVoteService)
+    private readonly service: PointVoteService,
+    private readonly accessService: AccessService,
+  ) {}
 
-    @UseGuards(AuthGuard('jwt'))
-    @Get('/')
-    async index(): Promise<PointVote[]> {
-        return this.service.findAll();
-    }
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/')
+  async index(): Promise<PointVote[]> {
+    return this.service.findAll();
+  }
 
-    @Get('/point/:id')
-    async findCommentsByPoint(@Param('id') id: string): Promise<PointVote[]> {
-        return this.service.findAllByPoint(+id);
-    }
+  @Get('/point/:id')
+  async findCommentsByPoint(@Param('id') id: string) {
+    return this.service.findAllByPoint(+id);
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Post('/:idPoint')
-    async create(
-        @Param('idPoint') idPoint: string,
-        @Headers('authorization') authorization: string,
-        @Ip() ip,
-        @Body() voteBody: CreatePointVoteDto,
-    ) {
-        const response = await this.service.create(idPoint, voteBody, authorization);
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/:idPoint')
+  async create(
+    @Param('idPoint') idPoint: string,
+    @Headers('authorization') authorization: string,
+    @Ip() ip,
+    @Body() voteBody: CreatePointVoteDto,
+  ) {
+    const response = await this.service.create(
+      idPoint,
+      voteBody,
+      authorization,
+    );
 
-        await this.accessService.create(AccessHelper.ACTION.ADDED, `point ${idPoint} Vote ${voteBody ? 'true' : 'false'}`, authorization, ip);
+    await this.accessService.create(
+      AccessHelper.ACTION.ADDED,
+      `point ${idPoint} Vote ${voteBody ? 'true' : 'false'}`,
+      authorization,
+      ip,
+    );
 
-        return response;
-    }
+    return response;
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Delete('/:idPoint')
-    async delete(
-        @Headers('authorization') authorization: string,
-        @Ip() ip,
-        @Param('idPoint') idPoint,
-    ) {
-        const response = await this.service.delete(+idPoint, authorization);
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/:idPoint')
+  async delete(
+    @Headers('authorization') authorization: string,
+    @Ip() ip,
+    @Param('idPoint') idPoint,
+  ) {
+    const response = await this.service.delete(+idPoint, authorization);
 
-        await this.accessService.create(AccessHelper.ACTION.DELETE, `vote point`, authorization, ip);
+    await this.accessService.create(
+      AccessHelper.ACTION.DELETE,
+      `vote point`,
+      authorization,
+      ip,
+    );
 
-        return response
-    }
+    return response;
+  }
 }
--- src/models/public/pointVote/pointVote.service.ts
+++ src/models/public/pointVote/pointVote.service.ts
@@ -6,78 +6,104 @@ import { PointVote } from './entities/pointVote.entity';
 
 @Injectable()
 export class PointVoteService {
-    constructor(
-        @InjectRepository(PointVote)
-        private readonly repository: Repository<PointVote>,
-        @Inject(TokenService)
-        private readonly TokenService: TokenService,
-    ) { }
+  constructor(
+    @InjectRepository(PointVote)
+    private readonly repository: Repository<PointVote>,
+    @Inject(TokenService)
+    private readonly TokenService: TokenService,
+  ) {}
 
-    async findAll(): Promise<PointVote[]> {
-        return this.repository.find();
+  async findAll(): Promise<PointVote[]> {
+    return this.repository.find();
+  }
+
+  async findAllByPoint(id: number) {
+    //   async findAllByPoint(id: number): Promise<PointVote[]> {
+    const responseTrue = this.repository.findAndCount({
+      select: {
+        vote: true,
+      },
+      where: {
+        point: Equal(id),
+        vote: Equal(true),
+      },
+    });
+
+    const responseFalse = this.repository.findAndCount({
+      select: {
+        vote: true,
+      },
+      where: {
+        point: Equal(id),
+        vote: Equal(false),
+      },
+    });
+
+    const response = await Promise.all([responseTrue, responseFalse]);
+
+    return { true: response[0][1], false: response[1][1] };
+  }
+
+  async findByPoint(point: {
+    id: number;
+    authorization?: string;
+    user?: number;
+  }): Promise<PointVote[]> {
+    if (point?.user) {
+      return this.repository.find({
+        where: {
+          point: Equal(point.id),
+          user: Equal(point.user),
+        },
+      });
     }
 
-    async findAllByPoint(id: number): Promise<PointVote[]> {
-        return this.repository.find({
-            select: {
-                vote: true,
-            },
-            where: {
-                point: Equal(id)
-            }
-        });
+    const objToken = await this.TokenService.findOne(point.authorization);
+
+    return this.repository.find({
+      where: {
+        point: Equal(point.id),
+        user: Equal(objToken.user),
+      },
+    });
+  }
+
+  async create(idPoint, voteBody, authorization: string): Promise<PointVote> {
+    const objToken = await this.TokenService.findOne(authorization);
+
+    const objPointVote = await this.findByPoint({
+      id: idPoint,
+      user: objToken.user.id,
+    });
+
+    if (!objPointVote[0]) {
+      const newPointVote = new PointVote();
+      newPointVote.point = idPoint;
+      newPointVote.user = objToken.user;
+      newPointVote.vote = voteBody.vote;
+      return this.repository.save(newPointVote);
     }
 
-    async findByPoint(point: { id: number, authorization?: string, user?: number }): Promise<PointVote[]> {
+    const partialPointVote = await this.repository.preload({
+      ...objPointVote[0],
+    });
 
-        if (point?.user) {
-            return this.repository.find({
-                where: {
-                    point: Equal(point.id),
-                    user: Equal(point.user)
-                }
-            });
-        }
+    partialPointVote.vote = voteBody.vote;
 
-        const objToken = await this.TokenService.findOne(point.authorization);
+    return this.repository.save(partialPointVote);
+  }
 
-        return this.repository.find({
-            where: {
-                point: Equal(point.id),
-                user: Equal(objToken.user)
-            }
-        });
+  async delete(idPoint: number, authorization: string): Promise<PointVote> {
+    const objToken = await this.TokenService.findOne(authorization);
+
+    const objPointVote = await this.findByPoint({
+      id: idPoint,
+      user: objToken.user.id,
+    });
+
+    if (objToken.user !== objPointVote[0].user) {
+      throw new NotFoundException(`Not authorized`);
     }
-
-    async create(idPoint, voteBody, authorization: string): Promise<PointVote> {
-        const objToken = await this.TokenService.findOne(authorization);
-
-        const objPointVote = await this.findByPoint({ id: idPoint, user: objToken.user.id })
-
-        if (!objPointVote[0]) {
-            const newPointVote = new PointVote();
-            newPointVote.point = idPoint;
-            newPointVote.user = objToken.user;
-            newPointVote.vote = voteBody.vote;
-            return this.repository.save(newPointVote)
-        }
-
-        const partialPointVote = await this.repository.preload({ ...objPointVote[0] });
-
-        partialPointVote.vote = voteBody.vote;
-
-        return this.repository.save(partialPointVote)
-    }
-
-    async delete(idPoint: number, authorization: string): Promise<PointVote> {
-
-        const objToken = await this.TokenService.findOne(authorization);
-
-        const objPointVote = await this.findByPoint({ id: idPoint, user: objToken.user.id })
-
-        if (objToken.user !== objPointVote[0].user) {
-            throw new NotFoundException(`Not authorized`);
-        }
-        return this.repository.remove(objPointVote[0]);
-    }
+    return this.repository.remove(objPointVote[0]);
+  }
 }
