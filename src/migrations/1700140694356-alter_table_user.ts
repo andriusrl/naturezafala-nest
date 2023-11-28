778a7ff76eec18ad262a6c0bc10dbe9e26f3e8d0
Andrius <andrius.rochalazarino@gmail.com>
2023-11-27 21:14:36 -0400

    feat: cors and migration fix email user

--- src/migrations/1700140694356-alter_table_user.ts
+++ src/migrations/1700140694356-alter_table_user.ts
@@ -8,13 +8,13 @@ export class AlterTableUser1700140694356 implements MigrationInterface {
             
             INSERT INTO public."user"
                 (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(1, 'Andrius Administrador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andriusrl', '221133', 1, true);
+                VALUES(1, 'Andrius Administrador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.administrador@email.com', '221133', 1, true);
             INSERT INTO public."user"
                 (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(2, 'Andrius Moderador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andriusrl2', '221133', 2, true);
+                VALUES(2, 'Andrius Moderador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.moderador@email.com', '221133', 2, true);
             INSERT INTO public."user"
                 (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(3, 'Andrius Usuário', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andriusrl3', '221133', 1, true);
+                VALUES(3, 'Andrius Usuário', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius@email.com', '221133', 1, true);
         `)
     }
 
