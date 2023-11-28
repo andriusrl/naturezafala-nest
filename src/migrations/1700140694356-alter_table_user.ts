fe8b17c3ad21c931b382c1ba965cb35d37375f6b
Andrius <andrius.rochalazarino@gmail.com>
2023-11-28 08:16:31 -0400

    fix: migration type user example

--- src/migrations/1700140694356-alter_table_user.ts
+++ src/migrations/1700140694356-alter_table_user.ts
@@ -1,9 +1,8 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AlterTableUser1700140694356 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
             ALTER TABLE public."user" ADD status boolean NOT NULL;          
             
             INSERT INTO public."user"
@@ -14,13 +13,12 @@ export class AlterTableUser1700140694356 implements MigrationInterface {
                 VALUES(2, 'Andrius Moderador', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius.moderador@email.com', '221133', 2, true);
             INSERT INTO public."user"
                 (id, "name", birth_date, fone, cpf, email, "password", "type", "status")
-                VALUES(3, 'Andrius Usuário', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius@email.com', '221133', 1, true);
-        `)
-    }
+                VALUES(3, 'Andrius Usuário', '1993-12-23 00:00:00.000', '6799999999', '00100100101', 'andrius@email.com', '221133', 3, true);
+        `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`
-    `)
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+    `);
+  }
 }
--- src/models/public/user/dto/updateUser.dto.ts
+++ src/models/public/user/dto/updateUser.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 import { CreateUserDto } from './createUser.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiProperty()
   @IsNumber()
   id: number;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  type: number;
 }
